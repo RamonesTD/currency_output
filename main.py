a14a6086877b1ad775d6c630708ab7f56fa9a39e
ramonestd <romannmail.ru@gmail.com>
2021-03-23 15:04:06 +0300

    literally currency output

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
 
-data = requests.get('https://www.nbrb.by/api/exrates/rates/145')
+def currency_support():
+    values_cur_id = input('Введите Cur_ID валюты: ')
+    return values_cur_id
+
+#currency_id = currency_support()
+
+data = requests.get('https://www.nbrb.by/api/exrates/rates/USD', data = {'parammode':'2'})
 data_json = data.json()
 
 print(data_json['Cur_OfficialRate'])
