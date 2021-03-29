d7c14a4db01377c7a8077bd7bac979878f02d92f
ramonestd <romannmail.ru@gmail.com>
2021-03-29 14:50:32 +0300

    add fix

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import requests
 data = requests.get('https://www.nbrb.by/api/exrates/rates/145')
 data_json = data.json()
 
+print(data_json['Cur_Abbreviation'], end=': ')
 print(data_json['Cur_OfficialRate'])
