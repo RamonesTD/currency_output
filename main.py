cc42112ffdf4f6fa2c8de2464fed10442a70421b
ramonestd <romannmail.ru@gmail.com>
2021-03-31 13:30:36 +0300

    checks error

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
 
-data = requests.get('https://www.nbrb.by/api/exrates/rates/145')
-data_json = data.json()
+try:
 
-print(data_json['Cur_Abbreviation'], end=': ')
-print(data_json['Cur_OfficialRate'])
+    data = requests.get('https://www.nbrb.by/api/exrates/rates/145')
+    data_json = data.json()
+
+    print(data_json['Cur_Abbreviation'], end=': ')
+    print(data_json['Cur_OfficialRate'])
+
+except requests.exceptions.ConnectionError:
+    print('Error: no internet connection')
