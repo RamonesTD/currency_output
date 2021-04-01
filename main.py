3af7c493dffe1b9e42cd44410337d02a8e0c0854
ushenko-dmitry <dimaushenko@mail.ru>
2021-04-01 10:57:35 +0300

    make func for get currency data

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import requests, configparser
 
 getByCurrencyURL = 'https://www.nbrb.by/api/exrates/rates/{currency}?parammode=2'
 
+def getDataByCurrency(currency):
+    try:
+        data = requests.get(getByCurrencyURL.replace('{currency}', currency))
+        return data.json()
+    except requests.exceptions.ConnectionError:
+        print('Error: no internet connection')
+        return None
+
 config = configparser.ConfigParser()
 config.read('config')
 
@@ -9,16 +17,10 @@ list = []
 currency_alpha = (config["currency_alpha"]["value"])
 list = currency_alpha.replace(' ','').split(',')
 
-try:
-
-    for value in list:
-
-        data = requests.get(getByCurrencyURL.replace('{currency}', value))
-        data_json = data.json()
-
+for value in list:
+    data_json = getDataByCurrency(value)
+    if data_json is not None:
         print(data_json['Cur_Abbreviation'], end=': ')
         print(data_json['Cur_OfficialRate'])
 
-except requests.exceptions.ConnectionError:
-    print('Error: no internet connection')
 
